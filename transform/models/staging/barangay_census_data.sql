c4c0e016bbee3f079db60ee74a61b7c02271bfb0
aldencabajar <aldenacabajar@gmail.com>
2025-08-08 22:19:45 -0700

    init commit

---
+++ transform/models/staging/barangay_census_data.sql
@@ -0,0 +1,7 @@
+SELECT 
+    UPPER(barangay) as barangay, 
+    population::float as population,
+    TRIM(REGEXP_REPLACE(city_municipality, '[0-9]', '')) as city_municipality,
+    TRIM(REGEXP_REPLACE(province, '[0-9]', '')) as province,
+    TRIM(REGEXP_REPLACE(region, '[0-9]', '')) as region
+FROM {{ source('bronze', 'barangay_census_data') }}
