00fb2fcceb6b268709178f07bfdcc1668818125b
aldencabajar <aldenacabajar@gmail.com>
2025-08-13 18:30:57 -0700

    implement incremental strategy

--- transform/models/staging/psa_website/psa_barangay_census_data.sql
+++ transform/models/staging/psa_website/psa_barangay_census_data.sql
@@ -1,7 +1,30 @@
+{{
+    config(
+    materialized = 'incremental',
+    )
+}}
+
+WITH preproc AS (
+    SELECT
+        UPPER(barangay) as barangay, 
+        population::integer as population,
+        TRIM(REGEXP_REPLACE(city_municipality, '[0-9]', '')) as city_municipality,
+        TRIM(REGEXP_REPLACE(province, '[^A-Za-z-\s]', '', 'g')) as province,
+        TRIM(REGEXP_REPLACE(region, '[0-9]', '')) as region,
+        source_timestamp_utc,
+        source_uri,
+        NOW() AS updated_date_time_utc
+    FROM {{ source('bronze', 'psa_barangay_census_data') }} 
+
+    {% if is_incremental() %}
+    WHERE load_datetime_utc >= coalesce((select max(updated_date_time_utc) from {{ this }}), '1900-01-01')
+
+    {% endif %}
+
+)
+
 SELECT 
-    UPPER(barangay) as barangay, 
-    population::integer as population,
-    TRIM(REGEXP_REPLACE(city_municipality, '[0-9]', '')) as city_municipality,
-    TRIM(REGEXP_REPLACE(province, '[^A-Za-z-\s]', '', 'g')) as province,
-    TRIM(REGEXP_REPLACE(region, '[0-9]', '')) as region
-FROM {{ source('bronze', 'psa_barangay_census_data') }}
+    {{ dbt_utils.generate_surrogate_key(['barangay', 'source_uri']) }} as id,
+    preproc.*
+FROM preproc
+
--- transform/models/staging/psa_website/psa_geographical_codes.sql
+++ transform/models/staging/psa_website/psa_geographical_codes.sql
@@ -1,6 +1,19 @@
-select psgc::string as psgc,
+
+{{
+  config(
+    materialized = 'incremental',
+    )
+}}
+
+select psgc::string as geo_code,
 UPPER(TRIM(REGEXP_REPLACE(name, '\(.*\)', '', 'g'))) as name,
 UPPER(TRIM(geographic_level)) as geographic_level,
 source_uri,
-source_timestamp_utc
+source_timestamp_utc,
+NOW() AS updated_date_time_utc
+
 FROM {{ source('bronze', 'psa_geographical_codes') }}
+
+{% if is_incremental() %}
+  WHERE load_datetime_utc >= coalesce((select max(updated_date_time_utc) from {{ this }}), '1900-01-01')
+{% endif %}
