973674f827911ed6f729ea5cd0c4fe9dcbb18596
aldencabajar <aldenacabajar@gmail.com>
2025-08-15 23:09:51 -0700

    add dim barangay

--- transform/models/staging/psa_website/psa_geographical_codes.sql
+++ transform/models/staging/psa_website/psa_geographical_codes.sql
@@ -4,6 +4,7 @@ select
 psgc::string as geo_code,
 UPPER(TRIM(REGEXP_REPLACE(name, '\(.*\)', '', 'g'))) as name,
 UPPER(TRIM(geographic_level)) as geographic_level,
+UPPER(TRIM(urban_rural_class)) as urban_rural_class,
 source_uri,
 source_timestamp_utc,
 NOW() AS updated_date_time_utc
