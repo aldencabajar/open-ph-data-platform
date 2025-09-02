dfc5db157cf08d1449e16850c0540ff17bdc57f9
aldencabajar <aldenacabajar@gmail.com>
2025-09-01 17:53:56 -0700

    modify dim city municipality

--- transform/models/final/dim_city_municipality.sql
+++ transform/models/final/dim_city_municipality.sql
@@ -1,34 +1,13 @@
 
-WITH province as (
-    SELECT 
-        id, 
-        name as province_name,
-        geo_code as province_geo_code
-    FROM {{ ref('psa_geographical_codes') }}
-    WHERE geographic_level = 'PROV'
-)
-
-SELECT
-    cm.id,
-    p.id as province_id,
-    name as city_municipality_name,
-    geo_code,
+SELECT 
+    wcm.city_municipality_id as id,
+    p.city_municipality_name,
+    p.province_id,
     area_in_sqm,
     class
-FROM (
-    SELECT *,
-    SUBSTRING(geo_code, 1, LENGTH(geo_code) - 5) || '00000' as deriv_province_geo_code
-    FROM {{ ref('psa_geographical_codes') }}
-    WHERE geographic_level in ('CITY', 'MUN')
- ) cm
-LEFT JOIN province p
-ON cm.deriv_province_geo_code = p.province_geo_code
-LEFT JOIN {{ ref("wikipedia_city_municipality") }} wcm
-ON cm.name = wcm.city_municipality_name AND p.province_name = wcm.province_name
-
-
-
-
-
-
+FROM {{ ref('wikipedia_city_municipality__matched') }} wcm
+LEFT JOIN (
+    SELECT DISTINCT province_id, city_municipality_id, city_municipality_name
+    FROM {{ ref("psa_geographical_codes__pivoted") }}
+) p USING (city_municipality_id)
 
--- transform/models/intermediate/wikipedia_city_municipality__matched.sql
+++ transform/models/intermediate/wikipedia_city_municipality__matched.sql
@@ -1,39 +1,18 @@
 
 WITH province_city_municipality_geo AS (
     SELECT DISTINCT
-        province_id,
         province_name,
-        province_geo_code,
         city_municipality_id,
-        city_municipality_name,
-        city_municipality_geo_code
+        city_municipality_name
     FROM {{ ref('psa_geographical_codes__pivoted') }}
 )
 
 
-/*
 SELECT  
     wcm.* EXCLUDE (province_name, city_municipality_name),
     wcm.city_municipality_name,
     wcm.province_name,
-    COALESCE(pcmg.city_municipality_id, pcmg2.city_municipality_id) AS city_municipality_id,
-    COALESCE(pcmg.province_id, pcmg2.province_id) AS province_id
-FROM {{ ref('wikipedia_city_municipality') }} wcm
-LEFT JOIN province_city_municipality_geo pcmg
-ON wcm.city_municipality_name = pcmg.city_municipality_name
-AND wcm.province_name = pcmg.province_name
-
-LEFT JOIN province_city_municipality_geo pcmg2
-ON {{ normalize_city_name('pcmg2.city_municipality_name') }} LIKE CONCAT('%', {{ normalize_city_name('wcm.city_municipality_name') }}, '%')
-AND wcm.province_name = pcmg2.province_name
-*/
-
-SELECT  
-    wcm.* EXCLUDE (province_name, city_municipality_name),
-    wcm.city_municipality_name,
-    wcm.province_name,
-    pcmg.city_municipality_id AS city_municipality_id,
-    pcmg.province_id AS province_id
+    pcmg.city_municipality_id AS city_municipality_id
 FROM {{ ref('wikipedia_city_municipality') }} wcm
 LEFT JOIN province_city_municipality_geo pcmg
 ON {{ normalize_city_name('wcm.city_municipality_name') }} = {{ normalize_city_name('pcmg.city_municipality_name') }}
