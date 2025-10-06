6a6eefd7107ff740c7ca8c9f1b05e1b5b7e96b27
aldencabajar <aldenacabajar@gmail.com>
2025-10-05 23:42:18 -0700

    fix fact table

--- transform/models/final/fct_population.sql
+++ transform/models/final/fct_population.sql
@@ -4,9 +4,8 @@ SELECT
     cn.population,
     cn.census_year,
     cn.source_timestamp_utc
-FROM {{ ref('psa_barangay_census_data') }} AS cn
-LEFT JOIN {{ ref('psa_geographical_codes__pivoted') }} AS psgc
-    ON
-        cn.barangay = psgc.barangay_name
-        AND cn.city_municipality = psgc.city_municipality_name
-        AND cn.province = psgc.province_name
+FROM {{ ref('psa_geographical_codes__pivoted') }} AS psgc
+LEFT JOIN {{ ref('psa_barangay_census_data') }} AS cn
+    ON psgc.barangay_name = cn.barangay
+        AND psgc.city_municipality_name = cn.city_municipality
+        AND IFNULL(psgc.province_name, '') = IFNULL(cn.province, '')
